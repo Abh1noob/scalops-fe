6e1c3d3ac8c0597ec85a24853714ef50d208fa40
Abh1noob <abhinavganeshank@gmail.com>
2024-04-14 06:57:06 +0530

    feat: made signup signin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,31 +8,39 @@ import image1 from "../../public/assets/images/root/home.svg";
 
 export default function Home() {
   const router = useRouter();
-  
-  const handleSignUp = () => {};
-  const handleSignIn = () => {};
+
+  const handleSignUp = () => {
+    router.push("/signup");
+  };
+  const handleSignIn = () => {
+    router.push("/signin");
+  };
 
   return (
     <>
-      <Navbar />
-      <div className="w-[90vw] mx-auto">
-        <h1 className="self-start text-3xl  text-[#211A1D] my-6">
-          Grow With Us
-        </h1>
-        <h6 className="self-start text-base text-[#211A1D]">
-          ONDC Sellers platform which will break market like he broke yo mom
-        </h6>
-        <div className="flex gap-10 mt-8">
-          <PrimaryButton label="Get Started" onClick={handleSignUp} />
-          <PrimaryButton label="Sign In" onClick={handleSignIn} />
+      <div className="h-screen">
+        <Navbar />
+        <div className="w-[90vw] mx-auto ">
+          <h1 className="self-start text-3xl  text-[#211A1D] my-6">
+            Grow With Us
+          </h1>
+          <h6 className="self-start text-base text-[#211A1D]">
+            ONDC Sellers platform which will break market like he broke yo mom
+          </h6>
+          <div className="flex gap-10 mt-8 justify-center">
+            <PrimaryButton label="Get Started" onClick={handleSignUp} />
+            <PrimaryButton label="Sign In" onClick={handleSignIn} />
+          </div>
+          <div className="h-full mt-20 bg-black">
+            <Image
+              src={image1}
+              alt="image1"
+              height={0}
+              width={0}
+              className="h-auto w-[890vw] mx-auto mt-8 "
+            />
+          </div>
         </div>
-        <Image
-          src={image1}
-          alt="image1"
-          height={0}
-          width={0}
-          className="h-auto w-[890vw] mx-auto mt-8"
-        />
       </div>
     </>
   );
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,11 +1,113 @@
-import React from "react";
+"use client";
+import Navbar from "@/components/navbar";
+import axios from "axios";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+import { FaRegUser, FaEye, FaEyeSlash } from "react-icons/fa";
 
-const SingIn = () => {
+const SignIn = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const submitFunction = async () => {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_APIURL}/auth/sign-in`,
+      {
+        email: email,
+        password: password,
+      }
+    );
+  };
+  const handleSubmit = async () => {
+    toast.loading("Loading...");
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_APIURL}/auth/sign-in`,
+        {
+          email: email,
+          password: password,
+        }
+      );
+      toast.dismiss();
+      toast.success("Success!");
+      window.location.href = "/dashboard";
+    } catch (error: any) {
+      if (axios.isAxiosError(error) && error.response) {
+        toast.dismiss();
+        toast.error(`Error: ${error.response.status}`);
+      } else {
+        toast.dismiss();
+        toast.error(`Error: ${error}`);
+      }
+    }
+  };
   return (
-    <div>
-      <div></div>
-    </div>
+    <>
+      <Navbar />
+      <div className="m-8">
+        <div className="bg-white rounded-xl p-5">
+          <h1 className="self-start text-3xl  text-[#211A1D] mt-6">
+            Sign In
+          </h1>
+          <h6 className="self-start text-base  text-[#211A1D]/70 mb-6">
+            to continue to ScaleOps
+          </h6>
+          <div className="flex flex-col gap-4">
+            <div className="flex flex-row w-full items-center justify-center">
+              <div className="h-12 w-[15%] text-xl bg-white rounded-l-lg border-l-2 border-y-2 border-gray-200 placeholder-[#211a1d80] flex items-center justify-center">
+                <FaRegUser />
+              </div>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="h-12 w-[85%] focus:outline-none placeholder:text-[14px] bg-white rounded-r-lg  border-r-2 border-y-2 border-gray-200 placeholder-[#211a1d80]"
+                placeholder="Email"
+              ></input>
+            </div>
+
+            <div className="flex flex-row w-full items-center justify-center">
+              <div className="h-12 w-[15%] text-xl bg-white rounded-l-lg border-l-2 border-y-2 border-gray-200 placeholder-[#211a1d80] flex items-center justify-center">
+                <FaRegUser />
+              </div>
+              <input
+                type={showPassword ? "text" : "password"}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="h-12 w-[85%] focus:outline-none placeholder:text-[14px] bg-white border-y-2 border-gray-200 placeholder-[#211a1d80]"
+                placeholder="Password"
+              ></input>
+
+              <button
+                type="button"
+                onClick={togglePasswordVisibility}
+                className="text-gray-400 hover:text-gray-600 focus:outline-none rounded-r-lg border-r-2 border-y-2 h-12 pr-2"
+              >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </button>
+            </div>
+            <div
+              className="bg-[#8075FF] text-white font-montserrat h-10 w-full flex items-center justify-center rounded-[20px] p-2 font-medium text-sm cursor-pointer"
+              onClick={handleSubmit}
+            >
+              Continue
+            </div>
+            <div>
+              Don&apos;t have an account? &nbsp;
+              <a className="text-[#8075FF]" href="/signup">
+                Sign up
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
-export default SingIn;
+export default SignIn;
